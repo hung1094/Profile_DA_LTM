0a7fff7d5d23b1c8a3f70640a5989e84a0bc74aa
hung1094 <hoangmanhhung05052005@gamil.com>
2025-12-31 08:27:47 +0700

    Sửa tên tiếp tiếp tiếp

--- pages/profile.js
+++ pages/profile.js
@@ -64,7 +64,7 @@ export default function Profile() {
                 <div className="relative group">
                   <div className="absolute -inset-1 bg-gradient-to-r from-cyan-400 to-teal-400 rounded-full blur opacity-75 group-hover:opacity-100 transition duration-1000"></div>
                   <div className="relative w-48 h-48 md:w-52 md:h-52 rounded-full border-4 border-white shadow-2xl overflow-hidden bg-white">
-                    <Image src="/avt.png" alt="Avatar" fill className="object-cover" priority />
+                    <Image src="/avt.png" alt="Avatar" fill className="object-cover" profile />
                   </div>
                 </div>
 
